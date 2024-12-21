dda65ca1c910ec80dc5f99eb7b3b6317194b3886
Marius Hartner <a12128413@unet.univie.ac.at>
2024-12-21 15:14:37 +0100

    Implemented parsing of command line arguments

--- main.cpp
+++ main.cpp
@@ -1,24 +1,45 @@
 #include "gameoflife.h"
 
-int main() {
+struct Config {
+    std::string inputFile;
+    std::string outputFile;
+    int generations;
+    bool measure;
+};
 
-    std::string inputFilename = "";
-    std::string outputFilename = "";
+Config parseArguments(const int argc, const char* argv[]) {
+    Config config;
+    for (int i = 1; i < argc; i++) {
+        std::string arg = argv[i];
+        if (arg == "--load" && i + 1 < argc) {
+            config.inputFile = argv[++i];
+        } else if (arg == "--save" && i + 1 < argc) {
+            config.outputFile = argv[++i];
+        } else if (arg == "--generations" && i + 1 < argc) {
+            config.generations = std::stoi(argv[++i]);
+        } else if (arg == "--measure") {
+            config.measure = true;
+        }
+    }
+    return config;
+}
 
-    int generations = 100;
+int main(int argc, char* argv[]) {
 
-    std::vector<std::vector<char>> inputBoard = readBoardInputFile(inputFilename);
+    Config config = parseArguments(argc, argv);
+
+    std::vector<std::vector<char>> inputBoard = readBoardInputFile(config.inputFile);
 
     Board previousBoard(inputBoard);
 
     Board nextBoard(inputBoard);
 
-    for (int i = 0; i < generations; ++i) {
+    for (int i = 0; i < config.generations; ++i) {
         calculateNextBoard(previousBoard, nextBoard);
         std::swap(previousBoard, nextBoard);
     }
 
-    writeBoardOutputFile(previousBoard, outputFilename);
+    writeBoardOutputFile(previousBoard, config.outputFile);
 
     return 0;
 }
